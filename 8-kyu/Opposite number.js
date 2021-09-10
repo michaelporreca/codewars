4d6402bed8c8b8ca2d4dc6323a70cdfbf3d130a8
michaelporreca <michaelporreca@gmail.com>
2021-09-10 08:31:08 -0700

    Opposite number

---
+++ 8-kyu/Opposite number.js
@@ -0,0 +1,17 @@
+/*
+Description:
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+// Solution
+function opposite(number) {
+    let oppNum = 0 - number
+    return oppNum;
+  }
